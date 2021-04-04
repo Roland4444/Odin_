dc5b324658317747c2a8ee129f99730f410738d5
Roland4444 <turbopollux@rambler.ru>
2021-04-04 18:59:22 +0400

    parseDSL classadding

--- Odin-Abstractions/Odin.cs
+++ Odin-Abstractions/Odin.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-
 namespace Odin_Abstractions
 {
     public class Odin
     { 
         Func<string, string, string> marinafunc=(foo,bar) =>"Σ="+(Int32.Parse(foo)+Int32.Parse(bar)).ToString();
         Func<string, string, string> guest=(a, b)=>"";
-        Func<string, string, Func<string, string, string>, string> result = (foo, bar, F) =>            
-
-            $"<h1>{foo}</h1><br>h1>{bar}</h1><br>{F(foo, bar)}";
-        
-       
+        Func<string, string, Func<string, string, string>, string> result = (foo, bar, F) => $"<h1>{foo}</h1><br>h1>{bar}</h1><br>{F(foo, bar)}";
         public string callguest(){
             var foo = "3";
             var bar = "7";
@@ -20,11 +15,6 @@ namespace Odin_Abstractions
             var foo = "3";
             var bar = "7";
             return (result(foo,bar, marinafunc));
-        }
-    
-        public static void Main(string[] args){
-            var odin = new Odin(); 
-            Console.WriteLine(odin.callmarina());
         }
     }
 }
