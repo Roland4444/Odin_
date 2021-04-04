c006dd8eade7d870e4344eda126fd678bb05b37c
Roland4444 <turbopollux@rambler.ru>
2021-04-04 16:41:51 +0400

    first commit

---
+++ Odin-Abstractions/Odin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Odin_Abstractions
+{
+    public class Odin
+    { 
+        Func<string, string, string> marinafunc=(foo,bar) =>"Σ="+(Int32.Parse(foo)+Int32.Parse(bar)).ToString();
+        Func<string, string, string> guest=(a, b)=>"";
+        Func<string, string, Func<string, string, string>, string> result = (foo, bar, F) =>            
+
+            $"<h1>{foo}</h1><br>h1>{bar}</h1><br>{F(foo, bar)}";
+        
+       
+        public string callguest(){
+            var foo = "3";
+            var bar = "7";
+            return (result(foo,bar, guest));
+        }
+        public string callmarina(){
+            var foo = "3";
+            var bar = "7";
+            return (result(foo,bar, marinafunc));
+        }
+    
+        public static void Main(string[] args){
+            var odin = new Odin(); 
+            Console.WriteLine(odin.callmarina());
+        }
+    }
+}
